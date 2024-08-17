27c4c9fdc245b8943d289431d4fd19206c313c14
Patrick W. Crawford <theduckcow@live.com>
2024-08-17 09:09:35 -0700

    Some title menu work and started credits element

--- wheelie-big-and-small/main.gd
+++ wheelie-big-and-small/main.gd
@@ -1,6 +1,6 @@
 extends Control
 
+const MENU = "res://ui/menu.tscn"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://ui/menu.tscn")
+	get_tree().change_scene_to_file(MENU)
---
+++ wheelie-big-and-small/ui/credits.gd
@@ -0,0 +1,15 @@
+extends VBoxContainer
+
+const EXTERNAL_LIST = [
+	["8-bit Memphis Patterns Pack", "https://pixelbuddha.net/download/freebie/827-8-bit-memphis-patterns-pack"]
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
