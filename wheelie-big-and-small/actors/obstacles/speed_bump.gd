db9814a56f6bbd00d6cdf9e6f752babc581b83e9
Patrick W. Crawford <theduckcow@live.com>
2024-08-17 21:40:52 -0700

    Initial logic for obstacle and ending the run via signal

---
+++ wheelie-big-and-small/actors/obstacles/speed_bump.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("We hit a speed bump!")
+	if body is not PlayerCar:
+		return
+	var car:PlayerCar = body
+	if car.get_speed() > 5:
+		car.set_speed(5)
